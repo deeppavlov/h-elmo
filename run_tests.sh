780bbca9a711dcd5ece9fa369989102b4b56f8f8
Anton Peganov <peganov@phystech.edu>
2019-06-06 17:01:00 +0300

    refactor run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -6,12 +6,22 @@ dpenv
 
 setdev 0
 
-python3 $TT ~/h-elmo/tests/experiments/correlation/nocorrloss/batch_mean/100_100short.json --test --no_logging
-python3 $TT ~/h-elmo/tests/experiments/correlation/nocorrloss/overfitting/validate_on_train.json --test --no_logging
-python3 $TT ~/h-elmo/tests/experiments/residual/nocorrloss/batch_mean/100_100short.json --test --no_logging
-python3 $TT ~/h-elmo/tests/experiments/residual/nocorrloss/overfitting/validate_on_train.json --test --no_logging
-python3 $TT ~/h-elmo/tests/experiments/resrnn/no_matrix_dim_adjustment.json --test --no_logging
-python3 $TT ~/h-elmo/tests/experiments/residual/straight1.json --test --no_logging
+tt=("$HOME/h-elmo/tests/experiments/correlation/nocorrloss/batch_mean/100_100short.json" \
+    "$HOME/h-elmo/tests/experiments/correlation/nocorrloss/overfitting/validate_on_train.json" \
+    "$HOME/h-elmo/tests/experiments/residual/nocorrloss/batch_mean/100_100short.json" \
+    "$HOME/h-elmo/tests/experiments/residual/nocorrloss/overfitting/validate_on_train.json" \
+    "$HOME/h-elmo/tests/experiments/resrnn/no_matrix_dim_adjustment.json" \
+    "$HOME/h-elmo/tests/experiments/residual/straight1.json")
 
-python3 $HS ~/h-elmo/tests/experiments/resrnn/small/char/hp/200.json --test --no_logging
-python3 $HS ~/h-elmo/tests/experiments/resrnn/residual/200.json --test --no_logging
+hs=("$HOME/h-elmo/tests/experiments/resrnn/small/char/hp/200.json" \
+    "$HOME/h-elmo/tests/experiments/resrnn/residual/200.json")
+
+for config in ${tt[@]}; do
+  echo "Processing" $config
+  python3 $TT $config --test --no_logging
+done
+
+for config in ${tt[@]}; do
+  echo "Processing" $config
+  python3 $HS $config --test --no_logging
+done
