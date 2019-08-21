681cd306772635abc4cbd689c8b8b462e5ade58b
Anton Peganov <peganov@phystech.edu>
2019-08-21 15:10:37 +0300

    add commands for vary batch size and vary num unrollings corr exps

--- helmo/util/plot/commands.sh
+++ helmo/util/plot/commands.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+
 # for plotting loss and correlation in 100 100_100 500_500
 sorting_key="def sorting_key(x):"$'\n'"    return tuple(eval(x))" \
   source ${PLOT}/correlation_and_loss_plots.sh "[100]@[100, 100]@[500, 500]" 100@100_100@500_500 plots
 unset sorting_key
 
+
 # for plotting wide correlation
 cd ~/h-elmo/expres/correlation/nocorrloss
 source ${PLOT}/correlation_and_loss_plots.sh "dropout 0@dropout 0.2@dropout 0.4@dropout 0.7" \
   wide/0@wide/0.2@wide/0.4@wide/0.7 wide/plots/20
+
 
 # for plotting adam 100 100_100 500_500
 cd ~/h-elmo/expres/correlation/nocorrloss
@@ -17,10 +20,12 @@ sorting_key="def sorting_key(x):"$'\n'"    return tuple(eval(x))" \
   adam/100@batch_mean/100_100short@wide/0 adam/plots
 unset sorting_key
 
+
 # for plotting loss - correlation 100 100_100 500_500
 sorting_key="def sorting_key(x):"$'\n'"    return tuple(eval(x))" \
   source ${PLOT}/loss_corr_plot.sh "[100]@[100, 100]@[500, 500]" 100@100_100@500_500 plots
 unset sorting_key
+
 
 # for plotting loss - correlation 100 100_100 500_500 for 2 optimizers text8
 cd ~/h-elmo/expres/correlation/nocorrloss/text8
@@ -40,6 +45,7 @@ sorting_key="${sorting_key2}" \
   adam/100@adam/100_100@adam/500_500@sgd/100@sgd/100_100@sgd/500_500 plots
 unset sorting_key
 unset xselect_params
+
 
 # for plotting loss - correlation 100 100_100 500_500 for 2 optimizers text8
 # with shuffled axvspan
@@ -64,6 +70,7 @@ unset sorting_key
 unset xselect_params
 unset additional_artists_str
 
+
 # for plotting loss - correlation 100 100_100 500_500 for 2 optimizers enwiki1G
 cd ~/h-elmo/expres/correlation/nocorrloss
 sorting_key2="def sorting_key(x):
@@ -80,6 +87,7 @@ sorting_key="${sorting_key2}" \
   "[100] adam@[100, 100] adam@[500, 500] adam@[100] sgd@[100, 100] sgd@[500, 500] sgd" \
   adam/100@batch_mean/100_100short@wide/0@sgd/100@sgd/100_100@sgd/500_500 enwiki1G/plots
 unset sorting_key
+
 
 # for plotting loss - correlation 100 100_100 500_500 for 2 optimizers enwiki1G
 # with shuffled axvspan
@@ -102,6 +110,7 @@ sorting_key="${sorting_key2}" \
 unset sorting_key
 unset additional_artists_str
 
+
 # for plotting loss - corrrelation 100 100_100 500_500 for adam enwiki1G-text8
 cd ~/h-elmo/expres/correlation/nocorrloss
 sorting_key2="def sorting_key(x):
@@ -120,6 +129,7 @@ sorting_key="${sorting_key2}" \
   enwiki1G-text8/plots/adam
 unset sorting_key
 
+
 # for sgd
 cd ~/h-elmo/expres/correlation/nocorrloss
 sorting_key2="def sorting_key(x):
@@ -136,6 +146,7 @@ sorting_key="${sorting_key2}" \
   "[100] enwiki1G@[100, 100] enwiki1G@[500, 500] enwiki1G@[100] text8@[100, 100] text8@[500, 500] text8" \
   sgd/100@sgd/100_100@sgd/500_500@text8/sgd/100@text8/sgd/100_100@text8/sgd/500_500 enwiki1G-text8/plots/sgd
 unset sorting_key
+
 
 # for plotting loss - corrrelation 100 100_100 500_500 for adam enwiki1G-text8
 # with shuffled vspans
@@ -159,6 +170,7 @@ sorting_key="${sorting_key2}" \
 unset sorting_key
 unset additional_artists_str
 
+
 # for sgd
 cd ~/h-elmo/expres/correlation/nocorrloss
 sorting_key2="def sorting_key(x):
@@ -179,6 +191,7 @@ sorting_key="${sorting_key2}" \
 unset sorting_key
 unset additional_artists_str
 
+
 # for sgd vary learning rate loss - corr plots
 cd ~/h-elmo/expres/correlation/nocorrloss/vary_lr/text8/sgd/100
 lw_params=( "--linewidth" "1.0" )
@@ -196,6 +209,7 @@ unset sorting_key
 unset additional_artists_str
 unset lw_params
 
+
 # for sgd vary learning rate loss AND corr plots
 cd ~/h-elmo/expres/correlation/nocorrloss/vary_lr/text8/sgd/100
 sorting_key="def sorting_key(x):
@@ -205,4 +219,64 @@ sorting_key="def sorting_key(x):
 source ${PLOT}/correlation_and_loss_plots.sh \
   "learning rate 3@learning rate 1@learning rate 0.3@learning rate 0.1@learning rate 0.03@learning rate 0.01" \
   3@1@0.3@0.1@0.03@0.01 plots
+unset sorting_key
+
+
+# for sgd vary batch size loss - corr plots
+cd ~/h-elmo/expres/correlation/nocorrloss/vary_bs/text8/sgd/100
+lw_params=( "--linewidth" "1.0" )
+sorting_key2="def sorting_key(x):
+    words = x.split()
+    return -float(words[-1])
+"
+additional_artists_str=( "-a" \
+  "${EXPRES}/correlation/nocorrloss/shuffled/text8/noise_best_loss_axvspan.pickle" )
+sorting_key="${sorting_key2}" \
+  source ${PLOT}/loss_corr_plot.sh \
+  "batch size 1024@batch size 512@batch size 256@batch size 128@batch size 64@batch size 32@batch size 20@batch size 10" \
+  1024@512@256@128@64@32@20@10 plots
+unset sorting_key
+unset additional_artists_str
+unset lw_params
+
+
+# for sgd vary batch size loss AND corr plots
+cd ~/h-elmo/expres/correlation/nocorrloss/vary_bs/text8/sgd/100
+sorting_key="def sorting_key(x):
+    words = x.split()
+    return -float(words[-1])
+"
+source ${PLOT}/correlation_and_loss_plots.sh \
+  "batch size 1024@batch size 512@batch size 256@batch size 128@batch size 64@batch size 32@batch size 20@batch size 10" \
+  1024@512@256@128@64@32@20@10 plots
+unset sorting_key
+
+
+# for sgd vary number of unrollings loss - corr plots
+cd ~/h-elmo/expres/correlation/nocorrloss/vary_unr/text8/sgd/100
+lw_params=( "--linewidth" "1.0" )
+sorting_key2="def sorting_key(x):
+    words = x.split()
+    return -float(words[-1])
+"
+additional_artists_str=( "-a" \
+  "${EXPRES}/correlation/nocorrloss/shuffled/text8/noise_best_loss_axvspan.pickle" )
+sorting_key="${sorting_key2}" \
+  source ${PLOT}/loss_corr_plot.sh \
+  "sequence length 1000@sequence length 400@sequence length 200@sequence length 100@sequence length 50@sequence length 20@sequence length 10@sequence length 5" \
+  1000@400@200@100@50@20@10@5 plots
+unset sorting_key
+unset additional_artists_str
+unset lw_params
+
+
+# for sgd vary number of unrollings loss AND corr plots
+cd ~/h-elmo/expres/correlation/nocorrloss/vary_unr/text8/sgd/100
+sorting_key="def sorting_key(x):
+    words = x.split()
+    return -float(words[-1])
+"
+source ${PLOT}/correlation_and_loss_plots.sh \
+  "sequence length 1000@sequence length 400@sequence length 200@sequence length 100@sequence length 50@sequence length 20@sequence length 10@sequence length 5" \
+  1000@400@200@100@50@20@10@5 plots
 unset sorting_key
