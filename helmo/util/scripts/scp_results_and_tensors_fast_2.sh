dab718249a3937d6838bb39f455f7e9f5c3bd75e
PeganovAnton <peganoff2@mail.ru>
2019-10-24 14:33:57 +0300

    Update copy script description

--- helmo/util/scripts/scp_results_and_tensors_fast_2.sh
+++ helmo/util/scripts/scp_results_and_tensors_fast_2.sh
@@ -4,14 +4,33 @@
 # from lab servers to local machine. It reads experiments directories
 # names from standard input and puts them into file 'dirs_for_copy.txt'.
 # File 'dirs_for_copy.txt' is copied to server where it is used for forming
-# a list of files 'files_for_copy.txt'. The list for of files is used for
+# a list of files 'files_for_copy.txt'. The list of files is used for
 # creation of tar.gz archive 'for_copy.tar.gz' which is then passed to
 # local machine and unpacked.
 # Args:
-#     1. Path to directory relative to which archive is formed. Paths to experiments
+#     1. Path to directory relative to which archive is formed. Paths to
+#        experiments
 #        have to be given relative to this directory. Path to this directory is
 #        given relative to expres
-# Paths to experiments are give relative to directory provided as first argument to this script
+# Paths to experiments are given relative to directory provided as first
+# argument to this script
+#
+# Pipe `for d in "${dirs[@]}"; do echo $d; done | source the_script.sh`
+# is one of possible solutions.
+# Using source command is obligatory because the script needs aliases defined
+# in .bashrc
+#
+# Example
+# for ds in enwiki1G text8;do for opt in sgd adam; do for nn in 100 100_100 \
+#     500_500; do echo ${ds}/${opt}/${nn}; done; done; done | source \
+#     /home/anton/h-elmo/helmo/util/scripts/scp_results_and_tensors_fast_2.sh \
+#     hidden_state_rms
+#
+# `ssh-agent` in remote host.
+# ```
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_rsa
+# ```
 
 
 function main() {
