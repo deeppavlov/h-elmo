b24c50f4c50a2a32c4c1e24815247d7a70ea5c1b
PeganovAnton <peganoff2@mail.ru>
2019-10-28 10:42:38 +0300

    Add new commands for plotting

--- helmo/util/plot/commands.sh
+++ helmo/util/plot/commands.sh
@@ -717,3 +717,37 @@ for ds in enwiki1G text8; do
 done
 
 
+# Average MNIST tensors nc-ff
+cd ~/nc-ff/results
+for f in ff2_adam ff2_adam_sh ff2_sgd ff2_sgd_sh; do
+  mkdir ${f}/stats
+  for tensor in hs0_corr hs0_rms hs1_corr hs1_rms; do
+    d=${f}/stats/${tensor}
+    mkdir d
+    python3 ${SCRIPTS}/average_pickle_values.py ${f}/{0..9}/tensors/${tensor}.pickle \
+        --mean ${d}/mean.pickle --stddev ${d}/stddev.pickle \
+        --stderr_of_mean ${d}/stdder_of_mean.pickle --preprocess "np.sqrt({array})"
+  done
+  python3 ${SCRIPTS}/average_txt.py ${f}/{0..9}/results/valid/loss.txt -o ${f}/stats/loss.txt
+done
+
+
+# Draw nc-ff plots
+cd ~/nc-ff/results
+mkdir plots
+step_file=ff2_adam/0/results/valid/loss.txt
+tensors=(hs0_corr hs0_rms hs1_corr hs1_rms)
+ylabels=("mean square correlation" "mean square element" "mean square correlation" "mean square element")
+for i in {0..3}; do
+    python3 ${PLOT}/plot_data_from_pickle.py -l adam sgd \
+        -s "${step_file}" "${step_file}" -m "ff2_adam/stats/${tensors[i]}/mean.pickle" \
+        "ff2_sgd/stats/${tensors[i]}/mean.pickle" -d "ff2_adam/stats/${tensors[i]}/stddev.pickle" \
+        "ff2_sgd/stats/${tensors[i]}/stddev.pickle" -n -o "plots/adam-sgd/${tensors[i]}_plot_data.pickle"
+    python3 ${PLOT}/plot_from_pickle.py "plots/adam-sgd/${tensors[i]}_plot_data.pickle" \
+        -x step -y "${ylabels[i]}" -X symlog -o "plots/adam-sgd/${tensors[i]}_plot" \
+        -t fill -d best -O -s png -r 900 -g -w both
+done
+python3 ${PLOT}/plot_data_from_txt.py ff2_adam/stats/loss.txt ff2_sgd/stats/loss.txt \
+    -l adam sgd -x 0 -y 1 -e 2 -o plots/adam-sgd/loss_plot_data.pickle -n
+python3 ${PLOT}/plot_from_pickle.py plots/adam-sgd/loss_plot_data.pickle \
+    -x step -y loss -X symlog -o plots/adam-sgd/loss_plot -t fill -d best -O -s png -r 900 -g -w both
